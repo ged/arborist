b9cd04d32086613f7d9cc5281eacd7f7f0d177b1
Michael Granger <ged@FaerieMUD.org>
2016-04-20 13:57:54 -0700

    Fix the observer setup in spec data

--- spec/data/observers/webservices.rb
+++ spec/data/observers/webservices.rb
@@ -6,9 +6,17 @@ require 'arborist'
 
 Arborist::Observer "Webservers" do
 	subscribe to: 'node.delta',
-		where: { type: 'service', port: 80, status: ['up', 'down'] }
+		where: {
+			type: 'service',
+			port: 80,
+			delta: { status: ['up', 'down'] }
+		}
 	subscribe to: 'node.delta',
-		where: { type: 'service', port: 443, status: ['up', 'down'] }
+		where: {
+			type: 'service',
+			port: 443,
+			delta: { status: ['up', 'down'] }
+		}
 
 	action do |uuid, event|
 		$stderr.puts "Webserver %s is DOWN (%p)" % [ event['data']['identifier'], event['data'] ]
